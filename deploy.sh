dfce9d0f1fcc2be0c5df4f7b61e0e070aa4c2447
040772 <salomonguttmann@gmail.com>
2025-07-11 17:07:16 +0200

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+docker build -t 040772/multi-client:latest -t 040772/multi-client:$SHA ./client/Dockerfile ./client
+docker build -t 040772/multi-server:latest -t 040772/multi-server:$SHA ./server/Dockerfile ./server
+docker build -t 040772/multi-worker:latest -t 040772/multi-worker:$SHA ./worker/Dockerfile ./worker
+
+docker push 040772/multi-client:latest 
+docker push 040772/multi-server:latest 
+docker push 040772/multi-worker:latest 
+
+docker push 040772/multi-client:$SHA 
+docker push 040772/multi-server:$SHA 
+docker push 040772/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=040772/multi-server:$SHA
+kubectl set image deployments/client-deployment client=040772/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=040772/multi-worker:$SHA
+  
+  
